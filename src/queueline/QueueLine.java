942b0b0c541eb90dba30fc5308c1a2132d140808
Luciano Ferrari <lucianodarioferrari@gmail.com>
2019-04-07 15:14:45 -0300

    Add line queue

--- src/queueline/QueueLine.java
+++ src/queueline/QueueLine.java
@@ -11,34 +11,19 @@ In order to get the name of the person in the i position, the method getPersonNa
 
 import java.util.ArrayDeque;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class QueueLine {
-    public static void main(String[] args)
-    {
-        Queue<Integer> q = new LinkedList<>();
+    public static void main(String[] args){
+        int amount;
 
-        // Adds elements {0, 1, 2, 3, 4} to queue
-        for (int i=0; i<5; i++)
-            q.add(i);
+        try{
+            amount = Integer.parseInt(args[0]);
+        }catch(Exception e){
+            amount = 1;
+        }
 
-        // Display contents of the queue.
-        System.out.println("Elements of queue-"+q);
-
-        // To remove the head of queue.
-        int removedele = q.remove();
-        System.out.println("removed element-" + removedele);
-
-        System.out.println(q);
-
-        // To view the head of queue
-        int head = q.peek();
-        System.out.println("head of queue-" + head);
-
-        // Rest all methods of collection interface,
-        // Like size and contains can be used with this
-        // implementation.
-        int size = q.size();
-        System.out.println("Size of queue-" + size);
+        Queue q = new Queue();
+        q.enqueue(amount);
+        q.dequeue();
     }
 }
